aca912ff00199259a6f0bfcba87560359b1b77ce
boody <abdoarfat2006@gmail.com>
2025-03-05 14:30:43 +0200

    add github link

--- src/app/_components/navbar/Navbar.tsx
+++ src/app/_components/navbar/Navbar.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { FaGithub } from "react-icons/fa";
 import React, { useContext } from "react";
 import { useGSAP } from "@gsap/react";
 import { ThemContext } from "../context/Context";
 
 import Switch from "../toggles/Switch";
 import gsap from "gsap";
+import Link from "next/link";
 
 const Navbar = () => {
   useGSAP(() => {
@@ -23,7 +25,13 @@ const Navbar = () => {
     <header id="header" className={`shadow-sm w-full h-[80px] ${them} px-3 `}>
       <nav className="w-[90%] h-full mx-auto flex items-center justify-between">
         <h1 className="text-xl sm:text-2xl ">Portfolio</h1>
-        <Switch them={them} setThem={setThem} />
+        <div className="flex items-center gap-4" > 
+          <Link href={"https://github.com/abdelrhman-arfat"}>
+            <FaGithub className="text-2xl hover:bg-neutral-300 hover:text-black rounded-full duration-300 w-[30px] h-[30px] p-1"  />
+          </Link>
+
+          <Switch them={them} setThem={setThem} />
+        </div>
       </nav>
     </header>
   );
